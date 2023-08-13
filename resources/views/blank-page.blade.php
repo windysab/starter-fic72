8cef2f85aabcb0ed126565db56b3932ab104364d
Windy Sabtami <windysabtami92@gmail.com>
2023-08-13 15:07:27 +0800

    belajar

--- resources/views/blank-page.blade.php
+++ resources/views/blank-page.blade.php
@@ -12,7 +12,7 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Dashboard FIC 7 - Biznet GioCloud x Code with Bahri</h1>
+                <h1>Dashboard FIC 7 - Biznet GioCloud x Code with Bahri  xxx</h1>
                
             </div>
             <div class="row">
